d21e8000f331b68dbfd688501fcb153295967f2b
todd <todd@todd.(none)>
2015-06-09 17:57:04 +0500

    add search terms to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,15 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+
+  before_save :add_search_terms
+
+
+  private
+
+  def add_search_terms
+    self.search_terms = self.first_name.downcase + ' ' + self.last_name.downcase
+  end
+
 end
